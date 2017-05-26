9d86bfe381a7a70108b6a982cadfa46ff4ebe7a9
ryan <mail@interwebz.space>
2017-05-26 15:32:04 -0600

    Got this wrapped up. Just load the module tell it what phone number to send to and what message to send

--- txter.py
+++ txter.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
-
+import bandwidth_sdk
 from bandwidth_sdk import Client
 from mylogins import user, token, secret, orig
 from bandwidth_sdk import Message
 
+# init a client to get started with the api
+Client(user, token, secret)
+print "Below you will see your api creds with you are loading your mylogin.py file correctly"
 print "  user     " + user + "  token    " + token + "  secret    " + secret
 
-Client(user, token, secret)
+class Txtbox:
 
-Message.send(
-    sender='+17204647640',
-    receiver='+17206925355',
-    text='Alert - Motion Detected',
-    tag='test tag')
+    def sender(self, message, term):
 
-    # def checklen(ourmessage):
-    #     if len(ourmessage) >= 140:
-    #
+        Message.send(
+          sender='+17204647640',
+          receiver=term,
+          text=message,
+          tag='test tag')
+
+        return "Message sent successfully"
+
+txtthing = Txtbox()
